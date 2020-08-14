857658b451ffdef57ac3e8ba2d961b3ad2aae9ab
water <0529bill@gmail.com>
2020-08-14 21:08:05 +0800

    Updates

---
+++ precache-manifest.4d7f6c0a3d9f5088b55fcd036b54885f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd04fc266c2a32cd940ed93a7fdaf93",
+    "url": "/tic-tac-toe_game/index.html"
+  },
+  {
+    "revision": "b0bbc64cc4f399a27e3e",
+    "url": "/tic-tac-toe_game/static/css/main.c850e29a.chunk.css"
+  },
+  {
+    "revision": "ec282f8ef9cb1d7587a8",
+    "url": "/tic-tac-toe_game/static/js/2.45e04659.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe_game/static/js/2.45e04659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0bbc64cc4f399a27e3e",
+    "url": "/tic-tac-toe_game/static/js/main.b47173ee.chunk.js"
+  },
+  {
+    "revision": "37797c6a29a9a2974f0d",
+    "url": "/tic-tac-toe_game/static/js/runtime-main.00588804.js"
+  }
+]);
